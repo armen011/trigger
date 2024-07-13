cf126a3bed40db9249f92cdb270a0952d073c6bf
Armen <front2@greca.co>
2024-07-13 11:33:14 +0400

    fix: mobile view

--- app/Components/FirstSection/FirstSection.tsx
+++ app/Components/FirstSection/FirstSection.tsx
@@ -8,14 +8,14 @@ const FirstSection = () => {
     <Section
       ellipse="green"
       className="w-[746px] h-[755px] absolute -left-[200px] -top-1/3"
-      wrapperCN="w-full h-[calc(100vh-90px)] lg:h-[calc(100vh-104px)] xl:h-[calc(100vh-104px)] 2xl:h-[calc(100vh-136px)] relative"
+      wrapperCN="w-full h-[calc(100vh-104px)] lg:h-[calc(100vh-104px)] xl:h-[calc(100vh-104px)] 2xl:h-[calc(100vh-136px)] relative"
     >
       <TriggerIcon className="absolute h-6 left-4 top-6 md:h-8 md:left-10 md:top-7 xl:h-10 xl:left-20 xl:top-9 2xl:h-12 2xl:top-10 2xl:left-32" />
       <Image
         src="/images/head.png"
         alt="hero"
         layout="fill"
-        className="animate-smooth-appear max-h-[600px] mr-10 lg:mr-auto lg:max-h-[unset] max-w-[357px] md:max-w-[733px] mx-auto"
+        className="animate-smooth-appear max-h-[500px] mr-10 lg:mr-auto lg:max-h-[unset] max-w-[357px] md:max-w-[733px] mx-auto"
         objectFit="contain"
       />
       <Links />
