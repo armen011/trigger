0da8ce3be19720defaae2a5aae18578d71dbb874
Armen <front2@greca.co>
2024-07-14 16:40:11 +0400

    feat: add burger menu

--- components/Header/Links.tsx
+++ components/Header/Links.tsx
@@ -3,6 +3,8 @@ import LinkButton from "../LinkButton";
 import { links } from "./utils";
 import { motion } from "framer-motion";
 
+import { useState } from "react";
+
 const container = {
   visible: {
     transition: {
@@ -11,21 +13,24 @@ const container = {
   },
 };
 const Links = () => {
+  const [isBurgerMenuOpened, setIsBurgerMenuOpened] = useState(false);
   return (
-    <div className="hidden lg:block">
-      <motion.div
-        className="flex md:gap-8 2xl:gap-10"
-        variants={container}
-        initial="hidden"
-        animate="visible"
-      >
-        {links.map(({ href, title }, idx) => (
-          <LinkButton href={href} key={idx}>
-            {title}
-          </LinkButton>
-        ))}
-      </motion.div>
-    </div>
+    <>
+      <div className="hidden lg:block">
+        <motion.div
+          className="flex md:gap-8 2xl:gap-10"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          {links.map(({ href, title }, idx) => (
+            <LinkButton href={href} key={idx}>
+              {title}
+            </LinkButton>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
